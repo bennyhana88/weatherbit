99f2b0223c861924d9a69294c2f5be176c12dd3a
bennyhana88 <61158886+bennyhana88@users.noreply.github.com>
2023-09-10 17:47:47 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,46 @@
-let pressurePa = 0
+let pressure_at_sea_level_hPa = 1013.25  // Standard atmospheric pressure at sea level in hPa
+let pressure_hPa = 0
 let temp = 0
-let timestamp = 0
-let degrees = 0
-let pressurePsi = 0
-let humidityPercent = 0
-let windSpeedMph = 0
-let rainInches = 0
-let windDirection = ""
 let alt = 0
+let windDirection = ""
+let rainInches = 0
+let windSpeedMph = 0
+let humidityPercent = 0
+let pressurePsi = 0
+let degrees = 0
+let timestamp = 0
+let rawpressure = 0
 
 weatherbit.startWeatherMonitoring()
 // Redirect serial output to USB
 serial.redirectToUSB()
-
 // Initialization test
 serial.writeLine("Initialization successful")
 
 basic.forever(function () {
     timestamp = input.runningTime()
-
     // Collect all sensor readings concurrently
     temp = weatherbit.temperature() / 100
     serial.writeLine("Temp raw: " + temp)
     degrees = Math.round((temp * (9 / 5) + 32) * 100) / 100
     serial.writeLine("Degrees: " + degrees)
 
-    pressurePa = weatherbit.pressure()
-    serial.writeLine("Pressure Pa: " + pressurePa)
-    pressurePsi = Math.round(pressurePa * 0.00014503773779 * 100) / 100
+    // Convert pressure reading from Pa to hPa
+    pressure_hPa = Math.idiv(weatherbit.pressure(), 100)
+    serial.writeLine("Pressure hPa: " + pressure_hPa)
+
+    // Raw Pressure
+    rawpressure = weatherbit.pressure()
+    serial.writeLine("Raw Pressure: " + rawpressure)
+
+    // Altitude calculation using pressure_hPa
+    alt = 44330 * (1 - (pressure_hPa / pressure_at_sea_level_hPa) ** (1 / 5.255))
+    // Convert to feet
+    alt = Math.round(alt * 3.28084)
+    serial.writeLine("Altitude: " + alt)
+
+    // Convert hPa to PSI
+    pressurePsi = Math.round(pressure_hPa * 0.014503773773022 * 100) / 100
     serial.writeLine("Pressure PSI: " + pressurePsi)
 
     humidityPercent = Math.round(weatherbit.humidity() / 1024)
@@ -42,13 +55,8 @@ basic.forever(function () {
     windDirection = weatherbit.windDirection()
     serial.writeLine("Wind Direction: " + windDirection)
 
-    // Altitude calculation
-    let pressure_at_sea_level = 101325; // Replace this with the current sea level pressure for your location if known
-    alt = 44330 * (1 - Math.pow((pressurePa / pressure_at_sea_level), (1 / 5.255)));
-    alt = Math.round(alt * 3.28084); // Convert to feet
-    serial.writeLine("Altitude: " + alt)
-
     // Serialize the readings in CSV format and send over serial
-    serial.writeLine(`${timestamp}, ${degrees}, ${pressurePsi}, ${humidityPercent}, ${windSpeedMph}, ${rainInches}, ${windDirection}, ${alt}`)
-    basic.pause(1000) // 1 seconds
+    serial.writeLine("" + (`${timestamp}, ${degrees}, ${pressurePsi}, ${humidityPercent}, ${windSpeedMph}, ${rainInches}, ${windDirection}, ${alt}, ${rawpressure}`))
+
+    basic.pause(1000)
 })
