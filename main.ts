383c848672bd1e90485101b116f490ffc1f2e39a
bennyhana88 <61158886+bennyhana88@users.noreply.github.com>
2023-09-12 14:52:13 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,68 @@
-let pressure_hPa = 0
-let temp = 0
-let rawpressure = 0
-let timestamp = 0
-let degrees = 0
-let pressurePsi = 0
-let humidityPercent = 0
-let windSpeedMph = 0
-let rainInches = 0
-let windDirection = ""
-let alt = 0
-let meters = 0
-let pressureInHg = 0
-let pressure_at_sea_level_hPa = 1013.25
-weatherbit.startWeatherMonitoring()
-serial.redirectToUSB()
-serial.writeLine("Initialization successful")
+let timestamp = 0;
+let temp = 0;
+let degrees = 0;
+let meters = 0;
+let pressure_hPa = 0;
+let pressureInHg = 0;
+let rawpressure = 0;
+let alt = 0;
+let pressurePsi = 0;
+let humidityPercent = 0;
+let windSpeedMph = 0;
+let windDirection = "";
+let previousRain = 0;
+let rainInches = 0;
+let currentRain = 0;
+
+const pressure_at_sea_level_hPa = 1013.25;
+
+weatherbit.startWeatherMonitoring();
+serial.redirectToUSB();
+
+function calculateData() {
+    timestamp = input.runningTime();
+    temp = Math.round(weatherbit.temperature() / 100 * 10000) / 10000;
+    degrees = Math.round((temp * (9 / 5) + 32) * 10000) / 10000;
+    meters = Math.round(weatherbit.altitude() * 10000) / 10000;
+    pressure_hPa = Math.idiv(weatherbit.pressure(), 700);
+    pressureInHg = Math.round(pressure_hPa * 0.0295301 * 10000) / 10000;
+    rawpressure = Math.round(weatherbit.pressure() * 10000) / 10000;
+    alt = 44330 * 3.28084 * (1 - (pressure_hPa / pressure_at_sea_level_hPa) ** (1 / 5.255));
+    alt = Math.round((alt + 5280) * 10000) / 10000;
+    pressurePsi = Math.round(pressure_hPa * 0.014503773773022 * 10000) / 10000;
+    humidityPercent = Math.round(weatherbit.humidity() / 1024 * 10000) / 10000;
+    windSpeedMph = Math.round(weatherbit.windSpeed() * 0.621371 * 10000) / 10000;
+
+    // Handle the rain counter reset
+    currentRain = weatherbit.rain();
+    if (currentRain > previousRain) {
+        rainInches = Math.round((currentRain - previousRain) * 0.0110236 * 10000) / 1000;
+    } else {
+        rainInches = 0;
+    }
+    previousRain = currentRain;
+
+    windDirection = weatherbit.windDirection();
+}
+
+function createJSONData() {
+    return `{
+        "Timestamp": ${timestamp},
+        "Temperature (F)": ${degrees},
+        "Pressure (PSI)": ${pressurePsi},
+        "Humidity (%)": ${humidityPercent},
+        "Wind Speed (MPH)": ${windSpeedMph},
+        "Rain (inches)": ${rainInches},
+        "Wind Direction": "${windDirection}",
+        "Altitude": ${alt},
+        "Altitude (m)": ${meters},
+        "Raw Pressure": ${rawpressure},
+        "Pressure (inHg)": ${pressureInHg}
+    }`;
+}
+
 basic.forever(function () {
-    timestamp = input.runningTime()
-    temp = weatherbit.temperature() / 100
-    temp = Math.round(temp * 10000) / 10000
-    serial.writeLine("Temp raw: " + temp)
-    degrees = Math.round((temp * (9 / 5) + 32) * 10000) / 10000
-    serial.writeLine("Degrees: " + degrees)
-    meters = Math.round(weatherbit.altitude() * 10000) / 10000
-    serial.writeLine("Meters: " + meters)
-    pressure_hPa = Math.idiv(Math.idiv(weatherbit.pressure(), 100), 7)
-    serial.writeLine("Pressure hPa: " + pressure_hPa)
-    pressureInHg = Math.round(pressure_hPa * 0.0295301 * 10000) / 10000
-    serial.writeLine("Pressure inHg: " + pressureInHg)
-    rawpressure = Math.round(weatherbit.pressure() * 10000) / 10000
-    serial.writeLine("Raw Pressure: " + rawpressure)
-    alt = 44330 * 3.28084 * (1 - (pressure_hPa / pressure_at_sea_level_hPa) ** (1 / 5.255))
-    alt = Math.round(alt * 10000) / 10000
-    alt = alt + 5280
-    serial.writeLine("Altitude: " + alt)
-    pressurePsi = Math.round(pressure_hPa * 0.014503773773022 * 10000) / 10000
-    serial.writeLine("Pressure PSI: " + pressurePsi)
-    humidityPercent = Math.round(weatherbit.humidity() / 1024 * 10000) / 10000
-    serial.writeLine("Humidity: " + humidityPercent)
-    windSpeedMph = Math.round(weatherbit.windSpeed() * 0.621371 * 10000) / 10000
-    serial.writeLine("Wind Speed: " + windSpeedMph)
-    rainInches = Math.round(weatherbit.rain() * 0.0110236 * 10000) / 1000
-    serial.writeLine("Rain inches: " + rainInches)
-    windDirection = weatherbit.windDirection()
-    serial.writeLine("Wind Direction: " + windDirection)
-    serial.writeLine("" + (`${timestamp}, ${degrees}, ${pressurePsi}, ${humidityPercent}, ${windSpeedMph}, ${rainInches}, ${windDirection}, ${alt}, ${meters}, ${rawpressure}, ${pressureInHg}`))
-    basic.pause(1000)
-})
+    calculateData();
+    serial.writeLine(createJSONData());  // Directly passing the created JSON string
+    basic.pause(2000);
+});
